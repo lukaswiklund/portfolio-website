7a6051e761388627961a5b5ae2c0f34183319b89
Lukas Wiklund <lukas@wiklund.se>
2021-10-29 20:00:31 +0200

    Fixed SEO issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,32 +9,31 @@ import SkillsSection from "components/HeroSection/SkillsSection"
 import ContactSection from "components/HeroSection/ContactSection"
 
 const HomePage: NextPage = () => (
-	<NextSeo
-		title="Lukas Wiklund"
-		description="Hi, I'm Lukas Wiklund. A Software Engineer based in Gothenburg, Sweden."
-		openGraph={{
-			images: [{ url: "/lukas.png", width: 1536, height: 2048 }],
-		}}
-	>
-		<Container>
-			<Head>
-				<link rel="preconnect" href="https://fonts.gstatic.com" />
-				<link
-					rel="stylesheet"
-					href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800"
-				/>
-				<link
-					rel="stylesheet"
-					href="https://fonts.googleapis.com/css2?family=Overpass:ital,wght@0,100;0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,600;1,700;1,800;1,900"
-				/>
-			</Head>
+	<Container>
+		<NextSeo
+			title="Lukas Wiklund"
+			description="Hi, I'm Lukas Wiklund. A Software Engineer based in Gothenburg, Sweden."
+			openGraph={{
+				images: [{ url: "/lukas.png", width: 1536, height: 2048 }],
+			}}
+		/>
+		<Head>
+			<link rel="preconnect" href="https://fonts.gstatic.com" />
+			<link
+				rel="stylesheet"
+				href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800"
+			/>
+			<link
+				rel="stylesheet"
+				href="https://fonts.googleapis.com/css2?family=Overpass:ital,wght@0,100;0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,600;1,700;1,800;1,900"
+			/>
+		</Head>
 
-			<HeroSection />
-			<ExperienceSection />
-			<SkillsSection />
-			<ContactSection />
-		</Container>
-	</NextSeo>
+		<HeroSection />
+		<ExperienceSection />
+		<SkillsSection />
+		<ContactSection />
+	</Container>
 )
 
 const Container = styled.div`
