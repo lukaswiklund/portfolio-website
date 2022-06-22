cbf5fecec111968e359a69e1bb041b2499f3ef80
= <=>
2022-06-22 14:24:38 +0200

    Removed unused fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,15 +4,11 @@ import { Head, Html, Main, NextScript } from "next/document"
 const AppDocument: React.FC = () => (
 	<Html>
 		<Head>
-			<link rel="preconnect" href="https://fonts.gstatic.com" />
-			<link
-				rel="stylesheet"
-				href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800"
-			/>
-			<link
-				rel="stylesheet"
-				href="https://fonts.googleapis.com/css2?family=Overpass:ital,wght@0,100;0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,600;1,700;1,800;1,900"
-			/>
+			<link rel="preconnect" href="https://fonts.googleapis.com" />
+			<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+
+			<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans" />
+			<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Overpass:wght@400;900" />
 		</Head>
 		<body>
 			<Main />
