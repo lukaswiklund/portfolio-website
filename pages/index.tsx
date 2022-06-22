d3cdabb04fcd84ddf1a2f66cef19404b3beab0ae
= <=>
2022-06-22 14:08:33 +0200

    Fixed build error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import { NextPage } from "next"
-import Head from "next/head"
 import styled from "styled-components"
 import { NextSeo } from "next-seo"
 
@@ -17,17 +16,6 @@ const HomePage: NextPage = () => (
 				images: [{ url: "/lukas.png", width: 1536, height: 2048 }],
 			}}
 		/>
-		<Head>
-			<link rel="preconnect" href="https://fonts.gstatic.com" />
-			<link
-				rel="stylesheet"
-				href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800"
-			/>
-			<link
-				rel="stylesheet"
-				href="https://fonts.googleapis.com/css2?family=Overpass:ital,wght@0,100;0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,600;1,700;1,800;1,900"
-			/>
-		</Head>
 
 		<HeroSection />
 		<ExperienceSection />
