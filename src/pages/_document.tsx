3a659aa815a9f581da977c51cd790c23f0109d14
Lukas Wiklund <lukas@wiklund.se>
2024-08-14 19:55:29 +0200

    Removed Google Analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,18 +9,6 @@ const Document: React.FC = () => (
 
 			<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans" />
 			<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Overpass:wght@400;900" />
-
-			<script async src="https://www.googletagmanager.com/gtag/js?id=G-5TY0TVR0J8"></script>
-			<script
-				dangerouslySetInnerHTML={{
-					__html: `
-						window.dataLayer = window.dataLayer || [];
-						function gtag(){dataLayer.push(arguments);}
-						gtag('js', new Date());
-						gtag('config', 'G-5TY0TVR0J8');
-					`,
-				}}
-			/>
 		</Head>
 		<body>
 			<Main />
