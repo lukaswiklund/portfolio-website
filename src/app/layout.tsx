37b896f6c1fdf0ea4abe6721a2846176729c2554
Lukas Wiklund <hej@lukaswiklund.se>
2025-12-07 15:23:29 +0100

    Improve metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,9 @@ import { Metadata } from "next"
 export const metadata: Metadata = {
 	title: "Lukas Wiklund",
 	description: "Hi, I'm Lukas Wiklund. A Software Engineer based in Gothenburg, Sweden.",
-	openGraph: {
-		images: [{ url: "/lukas.png", width: 1536, height: 2048 }],
-	},
+	metadataBase: new URL("https://lukaswiklund.se"),
+	openGraph: { images: [{ url: "/lukas.png", width: 1536, height: 2048 }] },
+	alternates: { canonical: "/" },
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
